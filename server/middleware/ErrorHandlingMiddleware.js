7c11aa1607de5c8e923e4d2b036ea35d27bea039
nes <nes@MacBook-Pro-nes.local>
2024-04-12 16:09:48 +0300

    Drop files from .gitignore and create func registration

--- server/middleware/ErrorHandlingMiddleware.js
+++
@@ -1,9 +0,0 @@
-const ApiError = require('../error/ApiError')
-
-module.exports = function (err, req, res, next) {
-    if (err instanceof ApiError) { //instanceof проверка принадлежит ли объект err классу ApiError
-        return res.status(err.status).json({ message: err.message })
-    }
-
-    return res.status(500).json({ message: "Непредивденная ошибка" }) //если нет ошибке в классе ApiError
-}
