5967d6eca2d95cde8aad47c4fdaff238bec09dac
nes <nes@MacBook-Pro-nes.local>
2024-04-28 01:58:39 +0300

    add route to admin and logout

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,13 +4,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
-import { SHOP_ROUTE } from '../utils/consts';
-import {Button} from 'react-bootstrap';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
+import { Button } from 'react-bootstrap';
 import { observer } from 'mobx-react-lite';
+import { useNavigate } from 'react-router-dom';
 
 //observer для отслеживания статуса
-const NavBar = observer( () => {
+const NavBar = observer(() => {
     const { user } = useContext(Context)
+    const navigate = useNavigate();
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
@@ -18,8 +20,18 @@ const NavBar = observer( () => {
                 {/* проверка на авторизацию и отображение админки */}
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{ color: 'white' }}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className='ml-2'>Войти</Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => navigate(LOGIN_ROUTE)}
+                            className='ml-2'
+                        >
+                            Выйти</Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{ color: 'white' }}>
@@ -27,7 +39,7 @@ const NavBar = observer( () => {
                     </Nav>
                 }
             </Container>
-        </Navbar>
+        </Navbar >
     );
 });
 
